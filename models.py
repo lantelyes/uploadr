bdb5d18b2ccbeb1462f72895be171c03fede2223
Your Name <you@example.com>
2017-10-17 21:16:48 -0700

    start work on file dropdown descriptors

--- models.py
+++ models.py
@@ -1,11 +1,20 @@
 from flask import request
 from pymongo import MongoClient
 from flask_restful import Resource, Api
+from flask_uploads import UploadSet, configure_uploads, patch_request_class
 from datetime import datetime
 
 client = MongoClient()
 db = client.file_database
 file_collection = db.file_collection
+
+
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['txt'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 class File(Resource):
 
@@ -13,16 +22,22 @@ class File(Resource):
         return "Hello, world"
 
     def post(self):
-        size = request.args.get('size_in_kb')
-        name = request.args.get('name')
-        uri = request.args.get('uri')
 
+        name = request.args.get('name')
+        size = 0
+
+     #   file = request.files['file']
+     #   if file and is_allowed_extention(file.filename):
+      #      filename = secure_filename(filename)
+      #      path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+      #      file.save(path)
+       #     size = size = os.stat(path).st_size
+
+        #size = size = os.stat('/tmp/foo').st_size
 
         file_data = {
             'name': name,
             'size_in_Kb': size,
-            'is_folder': False,
-            'uri': uri,
             'date_created': str(datetime.now())
         }
 
