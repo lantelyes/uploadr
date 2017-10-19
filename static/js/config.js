fbcdf4ecadb781af7e96677081dbbbd5e0166818
Leo Antelyes <leo.antelyes@viasat.com>
2017-10-19 14:23:58 -0700

    tweak toaster config

--- static/js/config.js
+++ static/js/config.js
@@ -1,13 +1,12 @@
 app.config(function(toastrConfig) {
     angular.extend(toastrConfig, {
-      autoDismiss: false,
+      autoDismiss: true,
       containerId: 'toast-container',
       maxOpened: 0,    
       newestOnTop: true,
       positionClass: 'toast-top-center',
       preventDuplicates: false,
       preventOpenDuplicates: false,
-      target: 'body',
       progressBar: true
     });
   });
