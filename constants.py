c8559ac09ddd9ffcb836e240ffbe7edd509895ba
Leo Antelyes <leo.antelyes@viasat.com>
2017-10-20 12:20:15 -0700

    oops need the api key

--- constants.py
+++ constants.py
@@ -2,7 +2,8 @@ ALLOWED_EXTENSIONS = set(['pdf','doc','docx'])
 UPLOAD_FOLDER = 'data/'
 
 #Temporary for prototype
-#Ideally the final version would store user credentials in a secure database
+#Ideally the final version would store user credentials and key encrypted in a secure database
+SECRET_KEY = "shhh its a secret"
 USERS = {
     'atrium': 'atrium'
 }
