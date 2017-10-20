b00f319045b6648b2c148dafd10ba9908a8b77b5
Leo Antelyes <leo.antelyes@viasat.com>
2017-10-20 13:31:44 -0700

    update toastr config

--- static/js/config.js
+++ static/js/config.js
@@ -1,12 +1,14 @@
+//General configuration for modules
 app.config(function(toastrConfig) {
     angular.extend(toastrConfig, {
-      autoDismiss: true,
+      autoDismiss: false,
       containerId: 'toast-container',
       maxOpened: 0,    
       newestOnTop: true,
-      positionClass: 'toast-top-center',
+      positionClass: 'toast-bottom-center',
       preventDuplicates: false,
       preventOpenDuplicates: false,
-      progressBar: true
+      progressBar: true,
+      target: 'body'
     });
   });
